b9d5d98119da8b32e79e47ef9f0fac03e3333e91
czisa <czisasasa@gmail.com>
2021-04-23 18:43:14 -0400

    Random ideas python

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,18 @@
 import random
 def foo():
   print("Keep it logically awesome.")
+  f = open("quotes.txt","a")
+  f.write("Aguante linux\n")
+  f.write("Aguante Ubuntu\n")
+  f.write("Aguante Manjaro\n")
+  f.write("Aguante Pop os\n")
+  f.close()
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[random.randint(0,len(quotes)-1)])
+  new_quotes = [line.replace("\n",'') for line in quotes]
+  print(new_quotes[random.randint(0,len(new_quotes)-1)])
+  print(new_quotes[random.randint(0,len(new_quotes)-1)])
 
 if __name__== "__main__":
   foo()
